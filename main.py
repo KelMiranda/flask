d9facbdc2f3d2fb1f26ebc804d836d57aec162b5
Kelvin Miranda <kelmiranda88@icloud.com.com>
2023-11-29 23:42:44 -0600

    ashkajshd

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+#recordar estos comandos $env:FLASK_DEBUG=1  $env:FLASK_APP=main.py
+
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
@@ -6,5 +8,16 @@ if __name__ == '__main__':
     app.run(port = 5000, debug = True)
 
 @app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    respose = make_response(redirect('/hello'))
+    respose.set_cookie('user_ip', user_ip)
+
+    return respose
+    
+
+@app.route('/hello')
 def hello():
-    return 'Hello Flsask'
+    user_ip = request.cookies.get('user_ip')
+    return f'Hello usuario tu ip es: {user_ip}'
